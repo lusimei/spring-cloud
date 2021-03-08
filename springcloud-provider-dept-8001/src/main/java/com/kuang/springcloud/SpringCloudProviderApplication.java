9be73be7925c916eb1251f35ca6c119cfc856bd1
chad <chad@mieasy.com>
2021-03-08 11:23:59 +0800

    经测试dashboard 不能与feign一起使用

--- springcloud-provider-dept-8001/src/main/java/com/kuang/springcloud/SpringCloudProviderApplication.java
+++ springcloud-provider-dept-8001/src/main/java/com/kuang/springcloud/SpringCloudProviderApplication.java
@@ -4,6 +4,7 @@ import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServl
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -12,13 +13,6 @@ import org.springframework.context.annotation.Bean;
 @EnableEurekaClient
 @EnableDiscoveryClient
 public class SpringCloudProviderApplication {
-
-    @Bean
-    public ServletRegistrationBean hystrixMetricsStreamServlet() {
-        ServletRegistrationBean registration = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
-        registration.addUrlMappings("/actuator/hystrix.stream");
-        return registration;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudProviderApplication.class,args);
--- springcloud-provider-dept-hystrix-8004/src/main/java/com/kuang/springcloud/SpringCloudProviderHystrixApplication.java
+++ springcloud-provider-dept-hystrix-8004/src/main/java/com/kuang/springcloud/SpringCloudProviderHystrixApplication.java
@@ -14,6 +14,13 @@ import org.springframework.context.annotation.Bean;
 @EnableCircuitBreaker
 public class SpringCloudProviderHystrixApplication {
 
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        registration.addUrlMappings("/actuator/hystrix.stream");
+        return registration;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudProviderHystrixApplication.class,args);
     }
